7861a9dd6b9aae8fb5fcedad36f8a58fac5461ab
Mohamed Atta <mohamed.ah.atta@gmail.com>
2023-11-17 13:06:05 +0200

    Add core components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UsersComponent } from './components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/components/user-item/user-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-item.component.html',
+  styleUrl: './user-item.component.css'
+})
+export class UserItemComponent {
+
+}
