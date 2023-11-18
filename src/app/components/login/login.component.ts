b781b7fd220d6c18d70fec7cdc9dd61b4da57928
Mohamed Atta <mohamed.ah.atta@gmail.com>
2023-11-18 21:03:31 +0200

    Add basic login logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+  isSubmitted: boolean = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    public authService: AuthService
+  ) {}
+
+  onSubmit(): void {
+    console.log(
+      'submitted form',
+      this.registerForm.value,
+      this.registerForm.invalid
+    );
+    this.authService.login(
+      this.registerForm.value.email || '',
+      this.registerForm.value.password || ''
+    ).subscribe((res: any) => {
+      console.log('login response', res)
+    });;
+    this.isSubmitted = true;
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'https://reqres.in/api';
+
+  private _isAuthenticated$ = new BehaviorSubject<boolean>(false);
+  isAuthenticated$ = this._isAuthenticated$.asObservable();
+
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem('token');
+    this._isAuthenticated$.next(!!token);
+  }
+
+  login(email: string, password: string) {
+    const url = `${this.apiUrl}/login`;
+
+    return this.http.post(url, { email, password }).pipe(
+      tap((response: any) => {
+        this._isAuthenticated$.next(true);
+        localStorage.setItem('token', response.token);
+      })
+    );
+  }
+
+  logout() {
+    this._isAuthenticated$.next(false);
+    localStorage.removeItem('token');
+  }
+}
