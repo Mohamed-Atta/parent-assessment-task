590eafedf843ea03b1267423b7dc205e651666d5
Mohamed Atta <mohamed.ah.atta@gmail.com>
2023-11-19 02:52:59 +0200

    Wrap login logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, ButtonComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(public authService: AuthService, private router: Router) {}
+
+  onClickLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  onClickEnquire() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { ButtonComponent } from '../button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -19,20 +21,23 @@ export class LoginComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    public authService: AuthService
+    public authService: AuthService,
+    private router: Router
   ) {}
 
-  onSubmit(): void {
-    console.log(
-      'submitted form',
-      this.registerForm.value,
-      this.registerForm.invalid
+  isFormValid(): boolean {
+    return !Boolean(
+      this.registerForm.get('email')?.invalid ||
+        this.registerForm.get('password')?.invalid
     );
+  }
+
+  onSubmit(): void {
     this.authService.login(
       this.registerForm.value.email || '',
       this.registerForm.value.password || ''
     ).subscribe((res: any) => {
-      console.log('login response', res)
+      this.router.navigate(['/users'])
     });;
     this.isSubmitted = true;
   }
